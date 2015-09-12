99cdd4b1bddf6d508f0b42550da384006f905d24
n2omatt <n2o.matt@gmail.com>
2015-09-12 07:49:17 -0300

    Change the verification of comments or new lines to the get_all_lines_from_files method so now there is a unique place to check if the line is valid or not.

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ def install_ppa(ppa_lines):
     ppa_to_add = [];
 
     for line in ppa_lines:
-        line = line.replace("\n","").lstrip(" ").rstrip(" ");
-
         grep_cmd =  "grep ^ /etc/apt/sources.list ";
         grep_cmd += "/etc/apt/sources.list.d/* | grep {} > /dev/null";
         grep_cmd  = grep_cmd.format(line);
@@ -23,13 +21,13 @@ def install_ppa(ppa_lines):
             print "PPA ({}) already added.".format(line);
         else:
             ppa_to_add.append(line);
-    
 
-    for ppa_name in ppa_to_add:    
-        print "Adding PPA({})".format(ppa_name);        
+
+    for ppa_name in ppa_to_add:
+        print "Adding PPA({})".format(ppa_name);
         add_cmd = "sudo apt-add-repository ppa:{}".format(ppa_name);
         os.system(add_cmd);
-    
+
     if(len(ppa_to_add) != 0):
         os.system("sudo apt-get update && sudo apt-get upgrade");
 
@@ -40,19 +38,14 @@ def install_pkgs(install_lines):
     pkg_to_add = [];
 
     for pkg_name in install_lines:
-        if(len(pkg_name) == 0 or pkg_name[0] == "#"):
-            continue;
-
-        pkg_name = pkg_name.replace("\n","").lstrip(" ").rstrip(" ");
-        
         check_cmd = """dpkg -l | cut -d" " -f3 | grep ^{} > /dev/null""".format(pkg_name);
         ret_val = os.system(check_cmd);
-        
+
         if(ret_val == 0):
             print "Package ({}) already added".format(pkg_name);
         else:
             pkg_to_add.append(pkg_name);
-    
+
     for pkg_name in pkg_to_add:
         print "Install package ({})".format(pkg_name);
         install_cmd = "sudo apt-get install -y {}".format(pkg_name);
@@ -63,27 +56,22 @@ def install_pkgs(install_lines):
 ################################################################################
 def purge_pkgs(purge_lines):
     pkg_to_purge = [];
-    
-    for pkg_name in purge_lines:
-        pkg_name = pkg_name.replace("\n"," ").lstrip(" ").rstrip(" ");
-        
-        if(len(pkg_name) == 0 or pkg_name[0] == "#"):
-            continue;
 
+    for pkg_name in purge_lines:
         check_cmd = """dpkg -l | cut -d" " -f3 | grep ^{} > /dev/null""".format(pkg_name);
         ret_val = os.system(check_cmd);
-        
+
         if(ret_val == 0):
             pkg_to_purge.append(pkg_name);
         else:
             print "Package ({}) not installed".format(pkg_name);
-    
+
     for pkg_name in pkg_to_purge:
         print "Purging package ({})".format(pkg_name);
         purge_cmd = "sudo apt-get purge -y {}".format(pkg_name);
-        
+
         os.system(purge_cmd);
-              
+
 
 ################################################################################
 # Helper functions                                                             #
@@ -94,15 +82,17 @@ def get_all_lines_from_files(files_list):
         try:
             file_lines = open(filename).readlines();
             for line in file_lines:
+                line = line.replace("\n","").lstrip(" ").rstrip(" ");
                 if(len(line) != 0 and line[0] != "#"):
-                    lines.append(line);                
+                    lines.append(line);
         except Exception, e:
             print e;
             exit(1);
+
     return lines;
 
 def show_help():
-    print "Help";               
+    print "Help";
 
 ################################################################################
 # Script initializaton                                                         #
@@ -134,8 +124,8 @@ def main():
     install_lines = get_all_lines_from_files(install_filenames);
     purge_lines   = get_all_lines_from_files(purge_filenames);
     ppa_lines     = get_all_lines_from_files(ppa_filenames);
-    
-    install_ppa(ppa_lines);        
+
+    install_ppa(ppa_lines);
     install_pkgs(install_lines);
     purge_pkgs(purge_lines);
 
