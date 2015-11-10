9e8ee446d7e23e6bea4f38abd69b28e8e1de0567
n2omatt <n2o.matt@gmail.com>
2015-11-10 20:15:21 -0200

    Add the credential helper setup.

--- set_git_config.sh
+++ set_git_config.sh
@@ -3,3 +3,7 @@
 #Setup my username and email.
 git config --global user.email "n2o.matt@gmail.com"
 git config --global user.name  "n2omatt"
+
+#Set up the credential helper.
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600' #1hour
