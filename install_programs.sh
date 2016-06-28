287d145e8e7ff6264635a58d91151fd9c6e385ff
n2omatt <n2o.matt@gmail.com>
2016-06-27 23:10:23 -0300

    - Purge first and run post_run after all.

--- install_programs.sh
+++ install_programs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 #Add the ppas and install everything inside the install_base.
+./install_programs/main.py --purge=./install_programs/list_programs/purge.txt
 ./install_programs/main.py --ppa=./install_programs/list_programs/ppa.txt
 ./install_programs/main.py --install=./install_programs/list_programs/install.txt
+
+./install_programs/post_run.sh
