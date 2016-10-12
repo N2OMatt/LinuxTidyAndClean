701742e56fb324ea3b9f4ece7568cd6ce0c754a9
n2omatt <n2o.matt@gmail.com>
2016-10-12 17:44:20 -0300

    - Update the cocos2dx recipe to setup the libfmod.

--- binaries_info/cocos2dx.sh
+++ binaries_info/cocos2dx.sh
@@ -10,12 +10,12 @@
 
 
 DOWNLOAD_URL=http://cdn.cocos2d-x.org/cocos2d-x-3.13.1.zip
-DOWNLOAD_PATH="/home/n2omatt/Desktop"
+DOWNLOAD_PATH=$(mktemp -d)
 INSTALL_PATH="/home/n2omatt/Documents/Packages/cocos2d-x-3.13.1";
 
 ## Download.
 echo "Downloading...";
-# wget $DOWNLOAD_URL -P $DST_PATH;
+wget $DOWNLOAD_URL -P $DST_PATH;
 
 
 ## Uncompress
@@ -31,12 +31,23 @@ mkdir -p $INSTALL_PATH;
 
 mv -f $DOWNLOAD_PATH/cocos2d-x-3.13.1/* $INSTALL_PATH
 
+## Run the installation scripts.
 cd $INSTALL_PATH
 chmod 744 ./build/install-deps-linux.sh
 sudo ./build/install-deps-linux.sh
 
 chmod 744 ./setup.py
 ./setup.py
+
+
+## Link the FMOD lib to usr/local/lib
+##   I don't know why cocos2dx is using fmod in linux !!??
+echo "Copying / Linking FMOD..."
+sudo cp -f "$PWD/external/linux-specific/fmod/prebuilt/64-bit/libfmod.so"  /usr/local/lib/libfmod.so
+sudo cp -f "$PWD/external/linux-specific/fmod/prebuilt/64-bit/libfmodL.so" /usr/local/lib/libfmodL.so
+sudo ln -sf /usr/local/lib/libfmod.so /usr/local/lib/libfmod.so.6
+## Rebuild the cache
+sudo ldconfig;
 
 
 ## Restore the CWD
