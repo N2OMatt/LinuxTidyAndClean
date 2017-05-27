b9399be33642ae4d7fd42437f13110439b668759
n2omatt <n2o.matt@gmail.com>
2017-05-27 15:37:55 -0600

    - Update the install script to install the bash-status-line.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git submodule update --init --recursive
 ./set_git_config.sh
 ./download_all_repos.sh
 ./install_dots.sh
+./install_bash_status_line.sh
 ./install_programs.sh
 ./install_binaries.sh
 ./install_pip.sh
