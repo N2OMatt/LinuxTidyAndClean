b6eee785140e337f70fbeb6a37ac9987bac80350
n2omatt <n2o.matt@gmail.com>
2017-05-28 00:37:55 -0300

    - Add some scripts and order them in a logical way.

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 git submodule update --init --recursive
 
+./clean_home_folder.sh
+./create_bashprofile.sh
 ./set_git_config.sh
+./set_sublime_config.sh
+
 ./download_all_repos.sh
+./install_amazingcow_programs.sh
+
 ./install_dots.sh
 ./install_bash_status_line.sh
+
 ./install_programs.sh
 ./install_binaries.sh
 ./install_pip.sh
-./set_sublime_config.sh
 
