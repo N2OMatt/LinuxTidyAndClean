87d87f66df0b3c21ca9ac08af92477a98031af79
n2omatt <n2omatt@amazingcow.com>
2017-10-27 05:19:00 -0200

    - Commenting out the code since I can't test it right now.

--- installs/binaries.sh
+++ installs/binaries.sh
@@ -13,16 +13,18 @@
 ##   The actual installation stuff is inside of those scripts.                ##
 ##----------------------------------------------------------------------------##
 
-## COWNOTE(n2omatt): I'm setting it just for GNU/Linux for now, 
-##   the binaries scripts are set just to download the GNU/Linux
-##   version of the programs. Maybe we can get it running on 
-##   other OSes as well.  
-CURR_OS=$(uname -o | tr "[:upper:]" "[:lower:]" | tr  "/" "_");
-if [ "$CURR_OS" != "gnu_linux" ]; then
-    echo "[$0] OS ($CURR_OS) isn't gnu_linux - Aborting...";
-    exit 0;
-fi;
+## COWTODO(n2omatt): CHECK THIS....
 
-for INSTALL_FILE in $(ls ./binaries_info); do
-    sudo ./binaries_info/$INSTALL_FILE;
-done;
+# ## COWNOTE(n2omatt): I'm setting it just for GNU/Linux for now,
+# ##   the binaries scripts are set just to download the GNU/Linux
+# ##   version of the programs. Maybe we can get it running on
+# ##   other OSes as well.
+# CURR_OS=$(uname -o | tr "[:upper:]" "[:lower:]" | tr  "/" "_");
+# if [ "$CURR_OS" != "gnu_linux" ]; then
+#     echo "[$0] OS ($CURR_OS) isn't gnu_linux - Aborting...";
+#     exit 0;
+# fi;
+
+# for INSTALL_FILE in $(ls ./binaries_info); do
+#     sudo ./binaries_info/$INSTALL_FILE;
+# done;
