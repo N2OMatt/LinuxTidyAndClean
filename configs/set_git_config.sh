3dbc13b27d149abd04fd133d36baeee97a970341
n2omatt <n2omatt@amazingcow.com>
2017-10-27 05:25:16 -0200

    - Add the license headers.

--- configs/set_git_config.sh
+++ configs/set_git_config.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-##----------------------------------------------------------------------------##
-## File      : set_git_config.sh                                              ##
-## Project   : LinuxTidyAndClean                                              ##
-## Author    : n2omatt <n2omatt@amazingcow.com>                               ##
-## Date      : 2017                                                           ##
-## License   : GPLv3                                                          ##
-## Copyright : N2OMatt - Copyright (c) 2017                                   ##
+##~---------------------------------------------------------------------------##
+##                        ____                       _   _                    ##
+##                  _ __ |___ \ ___  _ __ ___   __ _| |_| |_                  ##
+##                 | '_ \  __) / _ \| '_ ` _ \ / _` | __| __|                 ##
+##                 | | | |/ __/ (_) | | | | | | (_| | |_| |_                  ##
+##                 |_| |_|_____\___/|_| |_| |_|\__,_|\__|\__|                 ##
+##                              www.n2omatt.com                               ##
+##  File      : set_git_config.sh                                             ##
+##  Project   : LinuxTidyAndClean                                             ##
+##  Date      : Oct 27, 2017                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : n2omatt <n2omatt@amazingcow.com>                              ##
+##  Copyright : n2omatt - 2017                                                ##
 ##                                                                            ##
-## Description:                                                               ##
-##   Initial configuration to git.                                            ##
-##----------------------------------------------------------------------------##
+##  Description :                                                             ##
+##                                                                            ##
+##---------------------------------------------------------------------------~##
 
+
+################################################################################
+## Script                                                                     ##
+################################################################################
 #Setup my username and email.
 git config --global user.email "n2omatt@amazingcow.com"
 git config --global user.name  "n2omatt"
