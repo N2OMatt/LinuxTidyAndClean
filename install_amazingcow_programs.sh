2f65f539a2e650382053c21dce756a65d5aecee6
n2omatt <n2omatt@amazingcow.com>
2017-10-27 01:46:40 -0200

    - Add the license header and cosmetic changes.

--- install_amazingcow_programs.sh
+++ install_amazingcow_programs.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-##----------------------------------------------------------------------------##
-## File      : install_amazingcow_programs.sh                                 ##
-## Project   : LinuxTidyAndClean                                              ##
-## Author    : n2omatt <n2omatt@amazingcow.com>                               ##
-## Date      : 2017                                                           ##
-## License   : GPLv3                                                          ##
-## Copyright : N2OMatt - Copyright (c) 2017                                   ##
+##~---------------------------------------------------------------------------##
+##                        ____                       _   _                    ##
+##                  _ __ |___ \ ___  _ __ ___   __ _| |_| |_                  ##
+##                 | '_ \  __) / _ \| '_ ` _ \ / _` | __| __|                 ##
+##                 | | | |/ __/ (_) | | | | | | (_| | |_| |_                  ##
+##                 |_| |_|_____\___/|_| |_| |_|\__,_|\__|\__|                 ##
+##                              www.n2omatt.com                               ##
+##  File      : install_amazingcow_programs.sh                                ##
+##  Project   : LinuxTidyAndClean                                             ##
+##  Date      : May 28, 2017                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : n2omatt <n2omatt@amazingcow.com>                              ##
+##  Copyright : n2omatt - 2017                                                ##
 ##                                                                            ##
-## Description:                                                               ##
-##   Installs the most usefuls AmazingCow's tools.                            ##
-##   To add /remove a tool just edit the PROGRAMS var.                        ##
-##----------------------------------------------------------------------------##
+##  Description :                                                             ##
+##    Installs the most usefuls AmazingCow's tools.                           ##
+##    To add /remove a tool just edit the PROGRAMS var.                       ##
+##---------------------------------------------------------------------------~##
 
+################################################################################
+## Vars                                                                       ##
+################################################################################
 AMAZINGCOW_DIR="$HOME/Documents/Projects/AmazingCow/AmazingCow-Tools/";
 PROGRAMS="Gosh                   \
           COWTODO                \
@@ -19,6 +28,9 @@ PROGRAMS="Gosh                   \
           GuardChecker           \
           License_Header_Checker"
 
+################################################################################
+## Script                                                                     ##
+################################################################################
 install_program()
 {
     echo "----> Installing $1...";
