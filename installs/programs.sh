177c45518dfcb2c5f7d4bf5b52256e25d66b2edb
n2omatt <n2omatt@amazingcow.com>
2017-10-27 05:02:45 -0200

    - Add the license header and fix the paths.

--- installs/programs.sh
+++ installs/programs.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
-##----------------------------------------------------------------------------##
-## File      : install_programs.sh                                            ##
-## Project   : LinuxTidyAndClean                                              ##
-## Author    : n2omatt <n2omatt@amazingcow.com>                               ##
-## Date      : 2017                                                           ##
-## License   : GPLv3                                                          ##
-## Copyright : N2OMatt - Copyright (c) 2017                                   ##
+##~---------------------------------------------------------------------------##
+##                        ____                       _   _                    ##
+##                  _ __ |___ \ ___  _ __ ___   __ _| |_| |_                  ##
+##                 | '_ \  __) / _ \| '_ ` _ \ / _` | __| __|                 ##
+##                 | | | |/ __/ (_) | | | | | | (_| | |_| |_                  ##
+##                 |_| |_|_____\___/|_| |_| |_|\__,_|\__|\__|                 ##
+##                              www.n2omatt.com                               ##
+##  File      : programs.sh                                                   ##
+##  Project   : LinuxTidyAndClean                                             ##
+##  Date      : Oct 27, 2017                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : n2omatt <n2omatt@amazingcow.com>                              ##
+##  Copyright : n2omatt - 2017                                                ##
 ##                                                                            ##
-## Description:                                                               ##
-##                                                                            ##
-##----------------------------------------------------------------------------##
+##  Description :                                                             ##
+##---------------------------------------------------------------------------~##
 
-## COWNOTE(n2omatt): This script just works for GNU/Linux since 
+################################################################################
+## Vars                                                                       ##
+################################################################################
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
+CURR_OS=$(uname -o | tr "[:upper:]" "[:lower:]" | tr  "/" "_");
+
+
+################################################################################
+## Script                                                                     ##
+################################################################################
+## COWNOTE(n2omatt): This script just works for GNU/Linux since
 ##    it'll install a lot of stuff from apt-get.
 ##    Actually it only works for Debian based OSes.
-CURR_OS=$(uname -o | tr "[:upper:]" "[:lower:]" | tr  "/" "_");
 if [ "$CURR_OS" != "gnu_linux" ]; then
     echo "[$0] OS ($CURR_OS) isn't GNU/Linux - Aborting...";
     exit 0;
 fi;
 
-./install_programs/pre_run.sh
+## The paths are relative to the script directory
+cd $SCRIPT_DIR;
 
-#Add the ppas and install everything inside the install_base.
-./install_programs/main.py --purge=./install_programs/list_programs/purge.txt
-./install_programs/main.py --ppa=./install_programs/list_programs/ppa.txt
-./install_programs/main.py --install=./install_programs/list_programs/install.txt
+./programs_info/pre_run.sh
 
-./install_programs/post_run.sh
+./programs_info/main.py --purge=./programs_info/list_programs/purge.txt
+./programs_info/main.py --ppa=./programs_info/list_programs/ppa.txt
+./programs_info/main.py --install=./programs_info/list_programs/install.txt
+
+./programs_info/post_run.sh
+
+## Restore the cwd.
+cd -;
