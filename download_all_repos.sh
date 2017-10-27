65748900776bdb700242436557c2c6b8fca158b9
n2omatt <n2omatt@amazingcow.com>
2017-10-27 01:44:19 -0200

    - Rewrite the script to improve the code.

--- download_all_repos.sh
+++ download_all_repos.sh
@@ -1,18 +1,57 @@
 #!/bin/bash
-##----------------------------------------------------------------------------##
-## File      : download_all_repos.sh                                          ##
-## Project   : LinuxTidyAndClean                                              ##
-## Author    : n2omatt <n2omatt@amazingcow.com>                               ##
-## Date      : 2017                                                           ##
-## License   : GPLv3                                                          ##
-## Copyright : N2OMatt - Copyright (c) 2017                                   ##
+##~---------------------------------------------------------------------------##
+##                        ____                       _   _                    ##
+##                  _ __ |___ \ ___  _ __ ___   __ _| |_| |_                  ##
+##                 | '_ \  __) / _ \| '_ ` _ \ / _` | __| __|                 ##
+##                 | | | |/ __/ (_) | | | | | | (_| | |_| |_                  ##
+##                 |_| |_|_____\___/|_| |_| |_|\__,_|\__|\__|                 ##
+##                              www.n2omatt.com                               ##
+##  File      : download_all_repos.sh                                         ##
+##  Project   : LinuxTidyAndClean                                             ##
+##  Date      : May 27, 2017                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : n2omatt <n2omatt@amazingcow.com>                              ##
+##  Copyright : n2omatt - 2017                                                ##
 ##                                                                            ##
-## Description:                                                               ##
-##                                                                            ##
-##----------------------------------------------------------------------------##
+##  Description :                                                             ##
+##     Downloads all Amazing Cow's and N2OMatt's repos.                       ##
+##---------------------------------------------------------------------------~##
 
-cd ./DownloadRepos;
-git pull origin master;
+################################################################################
+## Vars                                                                       ##
+################################################################################
+TARGET_DIR="$HOME/Documents/Projects/N2OMatt/DownloadRepos";
+GITHUB_URL="http://github.com/N2OMatt/DownloadRepos";
 
-./download_amazingcow.py 
-./download_n2omatt.py
+
+################################################################################
+## Script                                                                     ##
+################################################################################
+echo "--> Target directory:";
+echo "    $TARGET_DIR";
+
+## We already have the repos previously cloned...
+##   So maybe we just updating or clonning the missing ones.
+##   Let's go to the folder and start clean.
+if [ -d "$TARGET_DIR" ]; then
+    echo "--> Already have target directory.";
+    echo "    Cleanning it...";
+    cd "$TARGET_DIR";
+
+    git reset --hard master;
+    git pull origin master;
+
+## Otherwise clone it from github.
+else
+    echo "--> Doesn't have target directory.";
+    echo "    Clonning it...";
+    mkdir -pv $(dirname $TARGET_DIR);
+
+    cd $(dirname "$TARGET_DIR");
+    git clone "$GITHUB_URL";
+
+    cd "$TARGET_DIR";
+fi;
+
+## Start clone stuff!
+./download_all.sh
