247d2deb25d9ab814bffa91157f12072f2d48b79
n2omatt <n2omatt@amazingcow.com>
2018-02-04 03:33:51 -0200

    - Use the env value.

--- configs/create_bashprofile.sh
+++ configs/create_bashprofile.sh
@@ -22,8 +22,8 @@
 ################################################################################
 echo "--> Creating bash_profile...";
 
-echo "if [ -e ~/.bashrc ]; then " >  ~/.bash_profile;
-echo "   source ~/.bashrc"        >> ~/.bash_profile;
-echo "fi"                         >> ~/.bash_profile;
+echo "if [ -e ~/.bashrc ]; then " >  "$HOME/.bash_profile";
+echo "   source ~/.bashrc"        >> "$HOME/.bash_profile";
+echo "fi"                         >> "$HOME/.bash_profile";
 
 echo "--> Done...";
